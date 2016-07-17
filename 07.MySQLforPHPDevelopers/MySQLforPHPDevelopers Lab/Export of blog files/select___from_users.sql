7b4fb6dea94b1e879239ac563874c991989bd786
limerixtt123 <limerix@mail.bg>
2016-07-17 16:23:02 +0200

    Software Technology 2016

--- 07.MySQLforPHPDevelopers/MySQLforPHPDevelopers Lab/Export of blog files/select___from_users.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO blog.users (username, password, fullname) VALUES ('peter', '$2a$06$6hGkR4M363V5cWLqXpUsYO3PvRSuLjxjlZPM9hNgMD1kj5Oub.90S', 'Peter Marinov');
-INSERT INTO blog.users (username, password, fullname) VALUES ('maria', '$2a$06$6hGkR4M363V5cWLqXpUsYO3PvRSuLjxjlZPM9hNgMD1kj5Oub.90S', 'Maria Ivanova');
-INSERT INTO blog.users (username, password, fullname) VALUES ('admin', '$2a$06$6hGkR4M363V5cWLqXpUsYO3PvRSuLjxjlZPM9hNgMD1kj5Oub.90S', null);
-INSERT INTO blog.users (username, password, fullname) VALUES ('guest', null, null);
-INSERT INTO blog.users (username, password, fullname) VALUES ('ani', '$2a$06$6hGkR4M363V5cWLqXpUsYO3PvRSuLjxjlZPM9hNgMD1kj5Oub.90S', 'Ani Kirova');
-INSERT INTO blog.users (username, password, fullname) VALUES ('teo', '$2a$06$6hGkR4M363V5cWLqXpUsYO3PvRSuLjxjlZPM9hNgMD1kj5Oub.90S', 'Teodor Rusev');
-INSERT INTO blog.users (username, password, fullname) VALUES ('Greta', '3aeaaae99090a8585b2e7fd6c7b526cc', 'Greta Birdkljf');
